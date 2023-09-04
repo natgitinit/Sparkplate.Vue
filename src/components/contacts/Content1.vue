b5814b9025ee27e38f0c65b79c4547e640f85a5d
natgitinit <natgitinit@gmail.com>
2023-09-03 19:38:24 -0500

    component contact modal

---
+++ src/components/contacts/Content1.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input type="text" name="" required v-model="InputData" @input="handleInput('input-value', InputData)">
+        <label>Enter Content</label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ContentOne",
+    data() {
+        return { 
+            InputData: ''
+        }
+    },
+        methods: {
+        handleInput(emitEvent, value) {
+            this.$emit(emitEvent, value)
+        }
+    }
+}
+</script>
--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,63 +1,53 @@
 <template>
-  <div class="view">
+    <div class="view">
     <h1 class="view-name">Contacts</h1>
     <network-status />
     <h1>HELLO CONTACTS</h1>
     <!-- settings tabs -->
     <TabsWrapper class="flex items-center text-xl font-semibold">
-      <TabComponent
+        <TabComponent
         v-for="(tab, tabIndex) in tabs"
         :key="`settings-view_tab-${tabIndex}`"
         :on-click="() => (activeTab = tab)"
         :active="tab === activeTab"
         >{{ tab }}</TabComponent
-      >
+        >
     </TabsWrapper>
     <!-- settings tabs -->
 
-    <div class="bg-white shadow py-5 px-4 mt-4">
-      <!-- user content -->
-      <user-profile v-if="activeTab === 'user'" />
-      <!-- user content -->
+        <div class="bg-white shadow py-5 px-4 mt-4">
+            <!-- user content -->
+            <user-profile v-if="activeTab === 'user'" />
+            <!-- user content -->
 
-      <!-- security content -->
-      <security-settings v-if="activeTab === 'security'" />
-      <!-- security content -->
-
-      <!-- email content -->
-      <email-settings v-if="activeTab === 'email'" />
-      <!-- email content -->
-
-      <misc v-if="activeTab === 'misc'" />
+            <!-- security content -->
+            <security-settings v-if="activeTab === 'security'" />
+            <!-- security content -->
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 // Components
 import UserProfile from '@/components/settings/UserProfile.vue'
-import Misc from '@/components/settings/Misc.vue'
 import SecuritySettings from '@/components/settings/SecuritySettings.vue'
-import EmailSettings from '@/components/settings/EmailSettings.vue'
 import NetworkStatus from '@/components/settings/NetworkStatus.vue'
-// import Contacts from '@/components/settings/NetworkStatus.vue'
+// import Contacts from '@/components/contacts/ContactModal.vue'
 
 export default {
-  name: 'SettingsView',
-  components: {
-    UserProfile,
-    EmailSettings,
-    SecuritySettings,
-    Misc,
-    NetworkStatus
-  },
-  data: () => ({
-    activeTab: 'user',
-    tabs: ['user', 'security', 'email', 'misc']
-  }),
-  async mounted() {
-    this.activeTab = this.$route.params.activeTab
-  }
+    name: 'SettingsView',
+    components: {
+        UserProfile,
+        SecuritySettings,
+        NetworkStatus
+    },
+    data: () => ({
+        activeTab: 'user',
+        tabs: ['user', 'security', 'contacts']
+    }),
+    async mounted() {
+        this.activeTab = this.$route.params.activeTab
+    }
 }
 </script>
 
