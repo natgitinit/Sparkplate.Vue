3d7d7719d049f332424b47f94339f1860fa13b34
natgitinit <natgitinit@gmail.com>
2023-09-04 20:18:20 -0500

    modal QR code

--- src/components/contacts/ContactsModal.vue
+++ src/components/contacts/ContactsModal.vue
@@ -1,28 +1,36 @@
-
 <template>
   <transition name="modal-fade">
     <div class="modal-backdrop">
-      <div class="modal" role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription">
+      <div
+        class="modal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
         <header class="modal-header" id="modalTitle">
-          <slot name="header">
-            This is the default title!
-          </slot>
-          <button type="button" class="btn-close" @click="close" aria-label="Close modal">
+          <slot name="header"> This is the default title! </slot>
+          <button
+            type="button"
+            class="btn-close"
+            @click="close"
+            aria-label="Close modal"
+          >
             x
           </button>
         </header>
 
         <section class="modal-body" id="modalDescription">
-          <slot name="body">
-            This is the default body!
-          </slot>
+          <slot name="body"> This is the default body! </slot>
         </section>
 
         <footer class="modal-footer">
-          <slot name="footer">
-            This is the default footer!
-          </slot>
-          <button type="button" class="btn-green" @click="close" aria-label="Close modal">
+          <slot name="footer"> This is the default footer! </slot>
+          <button
+            type="button"
+            class="btn-green"
+            @click="close"
+            aria-label="Close modal"
+          >
             Close Modal
           </button>
         </footer>
@@ -32,16 +40,15 @@
 </template>
 
 <script>
-  export default {
-    name: 'ContactsModal',
-    methods: {
-      close() {
-        this.$emit('close');
-      },
-    },
-  };
+export default {
+  name: 'ContactsModal',
+  methods: {
+    close() {
+      this.$emit('close')
+    }
+  }
+}
 </script>
-
 
 <style>
 .modal-backdrop {
@@ -57,7 +64,7 @@
 }
 
 .modal {
-  background: #FFFFFF;
+  background: #ffffff;
   box-shadow: 2px 2px 20px 1px;
   overflow-x: auto;
   display: flex;
@@ -73,7 +80,7 @@
 .modal-header {
   position: relative;
   border-bottom: 1px solid #eeeeee;
-  color: #4AAE9B;
+  color: #4aae9b;
   justify-content: space-between;
 }
 
@@ -94,17 +101,19 @@
   right: 0;
   border: none;
   font-size: 20px;
-  padding: 10px;
+  padding: 50px;
   cursor: pointer;
   font-weight: bold;
-  color: #4AAE9B;
+  color: #4aae9b;
   background: transparent;
 }
 
 .btn-green {
   color: white;
-  background: #4AAE9B;
-  border: 1px solid #4AAE9B;
+  background: #4aae9b;
+  border: 1px solid #4aae9b;
   border-radius: 2px;
+  padding: 20px;
+  font-size: 18px;
 }
 </style>
--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,58 +1,60 @@
 <template>
   <div id="contacts-modal">
-    <button 
-      type="button"
-      class="btn"
-      @click="showModal"> IMPORT </button>
-  
-      <ContactsModal
-        v-show="isModalVisible"
-        @close="closeModal" >
-    
-        <template v-slot:header>
-          This is a new modal header. 
-        </template>
+    <button type="button" class="btn" @click="showModal">IMPORT</button>
 
-        <template v-slot:body>
+    <ContactsModal v-show="isModalVisible" @close="closeModal">
+      <template v-slot:header> This is a new modal header. </template>
 
-          This is a new modal body. 
-        </template>
+      <template v-slot:body>
+        <VueVCard
+          orgPost="00000"
+          orgStreet="Some Street name"
+          orgRegion="Some Region"
+          orgCity="Some City"
+          orgCountry="LRK"
+          orgName="OrganizationName"
+          firstName="John"
+          lastName="Doe"
+          workPhone="5555555555"
+          homePhone="5555555555"
+        />
+        This is a new modal body.
+      </template>
 
-        <template v-slot:footer>
-          This is a new modal footer. 
-        </template>
-      </ContactsModal>
+      <template v-slot:footer> This is a new modal footer. </template>
+    </ContactsModal>
 
     <Transition>
       <Modal :handle-close="closeModal" :is-opened="isModalVisible" />
     </Transition>
-
   </div>
 </template>
 
 <script>
 // Components
-import ContactsModal from '@/components/contacts/ContactsModal.vue';
+import ContactsModal from '@/components/contacts/ContactsModal.vue'
+import VueVCard from '@/components/contacts/VueVCard.vue'
 
 export default {
-    name: 'ContactsView',
-    components: {
-      ContactsModal
+  name: 'ContactsView',
+  components: {
+    ContactsModal,
+    VueVCard
+  },
+  data() {
+    return {
+      isModalVisible: false
+    }
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true
     },
-    data() {
-      return {
-        isModalVisible: false,
-      };
-    },
-    methods: {
-      showModal() {
-        this.isModalVisible = true;
-      },
-      closeModal() {
-        this.isModalVisible = false;
-      }
+    closeModal() {
+      this.isModalVisible = false
     }
   }
+}
 </script>
 
 <style lang="scss" scoped></style>
